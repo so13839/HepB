b7cb38412284290b98eedb6aa7e373b022e9edf2
Stephen Opiyo <so13839@gmail.com>
2023-10-09 19:14:55 -0400

    Add files via upload

--- server.R
+++ server.R
@@ -1,10 +1,25 @@
-server <- function(input, output) {
+server <- function(input, output, session) {
+  # Create an observer for the Reset button
+  observeEvent(input$resetButton, {
+    # Reset all input values to their initial states
+    updateSelectInput(session, "Gender", selected = "")
+    updateSliderInput(session, "AGE", value = 0)
+    updateSelectInput(session, "ADDRESS", selected = "")
+    updateSelectInput(session, "RH", selected = "")
+    updateSelectInput(session, "DONATION", selected = "")
+    updateSelectInput(session, "GROUP", selected = "")
+    updateSelectInput(session, "HCV", selected = "")
+    updateSelectInput(session, "HIV", selected = "")
+    updateSelectInput(session, "TPHA", selected = "")
+  })
+  
   observeEvent(input$predictButton, {
     # Create a new data frame with user inputs
     new_data <- data.frame(
       Gender = as.factor(input$Gender),
       AGE = input$AGE,
       GROUP = as.factor(input$GROUP),
+      ADDRESS = as.factor(input$ADDRESS),
       RH = as.factor(input$RH),
       DONATION = as.factor(input$DONATION),
       HCV = as.factor(input$HCV),
@@ -14,7 +29,7 @@ server <- function(input, output) {
     
     # Predict Hepatitis B using the GBM model with probabilities
     probabilities <- predict(model, new_data, type = 'response')
-    probabilities <- data.frame( probabilities)
+    probabilities <- data.frame(probabilities)
     names(probabilities) <- "REACTIVE"
     # Get the probability for being "REACTIVE"
     reactive_probability <- probabilities[,"REACTIVE"]
